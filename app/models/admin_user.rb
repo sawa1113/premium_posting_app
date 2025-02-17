540e4e68a01bf9aab7a2ef67eb1f2ab6195edb13
sawa1113 <63143529+sawa1113@users.noreply.github.com>
2025-02-17 16:36:09 +0900

    不要なメソッドの削除

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -4,14 +4,6 @@ class AdminUser < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # 認証メソッドの明示的な定義
-  def self.authenticate_admin_user!(email, password)
-    admin_user = find_by(email: email)
-    return admin_user if admin_user&.valid_password?(password)
-
-    false
-  end
-
   # 検索可能な属性をリストとして定義します
   def self.ransackable_attributes(auth_object = nil)
     ["email", "created_at", "updated_at", "reset_password_sent_at", "name"]
