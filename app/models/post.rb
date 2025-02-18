2f1ab6fdc1a6edccc75398bfcc6ba353e0387971
sawa1113 <63143529+sawa1113@users.noreply.github.com>
2025-02-18 15:16:16 +0900

    バリデーションの修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
     belongs_to :user, optional: true
 
     validates :title, presence: true
-    validates :description, length: { maximum: 100 }
+    validates :description, { presence: true, length: { maximum: 100 }}
 
     # Postモデルにおいて、投稿が作成された後に非同期でメール通知ジョブ(PostNotificationJob)を実行している
     after_create_commit { PostNotificationJob.perform_later(self) }
