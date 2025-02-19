b34be7d145911a017a62aa0842fcc17c4e90fdd1
sawa1113 <63143529+sawa1113@users.noreply.github.com>
2025-02-19 23:36:31 +0900

    ransackの導入と画面とボタン、コントローラーの修正

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'sassc'
 gem "cssbundling-rails", "~> 1.4"
 gem 'sidekiq'
 gem 'redis'
+gem 'ransack'
 
 # Use Redis adapter to run Action Cable in production
 # gem "redis", ">= 4.0.1"
--- app/models/post.rb
+++ app/models/post.rb
@@ -7,8 +7,13 @@ class Post < ApplicationRecord
     # Postモデルにおいて、投稿が作成された後に非同期でメール通知ジョブ(PostNotificationJob)を実行している
     after_create_commit { PostNotificationJob.perform_later(self) }
 
-    # 検索可能な属性をリストとして定義します
+    # 検索可能なカラム(属性)
     def self.ransackable_attributes(auth_object = nil)
+        ["title", "description"]
+    end
+
+    # 検索を許可する関連(associations)
+    def self.ransackable_associations(auth_object = nil)
         ["title", "description"]
     end
 end
